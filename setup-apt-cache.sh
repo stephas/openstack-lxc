684e4f792659b1e99e38eb1f6276ac0885cb71c6
Stephan <stephan@scheller.com>
2013-04-06 18:52:51 -0700

    typo in setup-apt-cache.sh

--- setup-apt-cache.sh
+++ setup-apt-cache.sh
@@ -4,7 +4,7 @@ if [ -s $filename ]; then
     cacheraddress=$(cat $filename)
     ping -c1 $cacheraddress
     if [ $? -eq 0 ]; then
-        echo using apt-cacher box $cacher_address
+        echo using apt-cacher box $cacheraddress
         echo 'Acquire::http::Proxy "http://'$cacheraddress':3142";' > /etc/apt/apt.conf.d/01proxy
         cache=1
     fi
